a762acd26a0ed2eac1d661251ff841a1a418afb0
Robert McCrary <robertmccrary@Roberts-MacBook-Pro.local>
2024-10-10 02:29:25 -0400

    started working on ast and parser

---
+++ ast/ast.go
@@ -0,0 +1,30 @@
+package ast
+
+import "monkey/token"
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+// recursive parsing
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
